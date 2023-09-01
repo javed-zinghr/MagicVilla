77eaeb96f8a38965b4f03e2e877d9b7a452d616a
Javed Ansari <142770073+javed-zinghr@users.noreply.github.com>
2023-09-01 12:34:57 +0530

    add all mathod

---
+++ MagicVilla_VillaApi/Data/VillaStore.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_VillaApi.Model.Dto;
+
+namespace MagicVilla_VillaApi.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO> {
+            new VillaDTO {Id=1, Name="Poolii View",Sqft=100,Occupancy=4},
+            new VillaDTO {Id=2, Name="Beachii View",Sqft=300,Occupancy=3}
+        };
+    }
+}
---
+++ MagicVilla_VillaApi/Model/Villa.cs
@@ -0,0 +1,10 @@
+﻿namespace MagicVilla_VillaApi.Model
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
